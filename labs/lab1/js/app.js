7e47e8bff7e228486a960dc35f47bc2aae158a98
Jamal Jennings <jamaljennings@Jamals-Air.attlocal.net>
2021-01-28 10:39:19 -0500

    add personal comp.

---
+++ labs/lab1/js/app.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(800, 600);
+
+  noStroke();
+
+  background(10, 144, 0);
+  fill(150, 62, 81);
+  rect(70, 60, 300, 50);
+
+  fill(235, 73, 199);
+  circle(100, 100, 30);
+
+  stroke(2);
+  line(10, 20, 200, 20);
+}
