f81cfa4d431266974ac68abe88d699925fd469d6
jatyjenn <jatyjenn@iu.edu>
2021-04-20 19:05:28 -0400

    m

--- labs/lab10/js/makeDivs.js
+++ labs/lab10/js/makeDivs.js
@@ -1,20 +1,2 @@
-let objects = [
 
-    { color: "#FF0000", height: 100, width: 300 },
-   
-    { color: "#FFFF00", height: 200, width: 200 },
-   
-    { color: "#ff0000", height: 300, width: 100 },
-   
-   ];
-
-   objects.forEach((obj) => {
-       let cont = document.createElement("cont");
-       div.setAttribute('color', obj.color);
-       div.setAttribute('height', obj.height);
-       div.setAttribute('width', obj.width);
-
-       document.body.appendChild(cont);
-
-   });
    
