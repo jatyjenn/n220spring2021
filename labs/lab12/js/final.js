0436245c6f34280532620d926efeee4067465f57
jatyjenn <jatyjenn@iu.edu>
2021-05-04 02:40:41 -0400

    k

--- labs/lab12/js/final.js
+++ labs/lab12/js/final.js
@@ -5,8 +5,8 @@ let grid = [
   ['','','']
 ]
 let players = ['X','O']
-let cont_time = false;
 let turn = 0;
+
 
 function setup() {
   createCanvas(3 * sz, 3 * sz);
@@ -39,31 +39,30 @@ function addplayers() {
   }
 }
 //this make the "O"
-function o(x,y) {
+function o (x,y) {
   let d = sz * 0.25;
   circle((sz / 2) + (sz * x),(sz / 2) + (sz * y),d);
 }
 // this  create an x
-function x(x,y){
+function x (x,y){
   let p = sz * 0.10;
   line((sz * x) + p,
        (sz * y) + p,
-       (sz * x + w) - p,
-       (sz * y + w) - p);
-  line((sz * x + w) - p,
+       (sz * x + sz) - p,
+       (sz * y + sz) - p);
+  line((sz * x + sz) - p,
        (sz * y) + p,
        (sz * x) + p,
-       (sz * y + w) - p);
+       (sz * y + sz) - p);
 }
 
-//this determines where the user clicked and places that user's symbol "x" or "o" in that spot. 
-
-//I learned about the floor() at https://genekogan.com/code/p5js-transformations/ this basically calulate the position of something( in this case a mouse click) and rounds it down to the nearest whole number
+//this determines where the user clicked and places that user's symbol "x" or "o" in that spot
+//I learned about the floor() at https://genekogan.com/code/p5js-transformations/ and on the p5js page seeing it on youtube. This basically calulate the position of something( in this case a mouse click) and rounds it down to the nearest whole number
 
 function mouseClicked() {
-  let x = floor(mouseX / sz);
-  let y = floor(mouseY / sz);
-  if (!cont_time && grid[x][y] == '') {
+  let x = floor (mouseX / sz);
+  let y = floor (mouseY / sz);
+  if (grid[x][y] == '') {
     if (turn == 0) {
       turn = 1;
     } else {
